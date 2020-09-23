fc1ba793863e3a30981fd77f55fe6de90f397f6a
xhazker <xhazker@gmail.com>
2020-09-23 15:42:18 +0300

    rename and save

--- autodownload.py
+++ autodownload.py
@@ -10,9 +10,10 @@ from selenium.webdriver.common.keys import Keys
 def download_anime(driver, anime):
     driver.get(anime[0])
     time.sleep(1)
+    l1 = []
     try:
         for i in range(int(anime[1]), int(anime[2])):
-            episode = driver.find_element_by_id('p'+str(i))
+            episode = driver.find_element_by_id('p'+str(i-1))
             actions = ActionChains(driver)
             actions.move_to_element(episode)
             actions.click(episode)
@@ -21,18 +22,21 @@ def download_anime(driver, anime):
             driver.switch_to.frame(driver.find_element_by_tag_name('iframe'))
             downlink = driver.find_element_by_link_text('720p (HD)').get_attribute('href')
             driver.get(downlink)
-            name = re.findall(r'\W720\W\d*.mp4', downlink)[0].replace('/','_') +'.crdownload'
-            print(name)
+            name = re.findall(r'\W720\W\d*.mp4', downlink)[0].replace('/','_')
+            print(name +'.crdownload')
             time.sleep(5)
-            print(os.path.exists('/home/xhazker/Downloads/'+name))
+            print(os.path.exists('/home/xhazker/Downloads/'+name +'.crdownload'))
             while(True):
-                if not os.path.exists('/home/xhazker/Downloads/'+name):
+                if not os.path.exists('/home/xhazker/Downloads/'+name +'.crdownload'):
                     break
                 time.sleep(1)
             driver.switch_to.default_content()
+            os.rename('/home/xhazker/Downloads/'+name, '/home/xhazker/Downloads/' + anime[3] + '-AnimeVosts-episode-'+str(i-1)+'.mp4')
+            l1.append(str(anime[3] + '-AnimeVosts-episode'+str(i-1)))
             print('WELL DONE!Next!')
     finally:
         driver.quit()
+    return l1
 
 
 if __name__ == ('__main__'):
@@ -40,5 +44,10 @@ if __name__ == ('__main__'):
     driver = webdriver.Chrome(PATH)
     with open('anime.csv', newline='') as csvfile:
         anime = list(csv.reader(csvfile, delimiter=';'))
-    download_anime(driver, anime[0])
+    l1 = download_anime(driver, anime[0])
+    with open('episodes.txt', 'w', encoding='utf-8') as g:
+    	for i in l1:
+            print(i,file=g)
+    	
+    
 
